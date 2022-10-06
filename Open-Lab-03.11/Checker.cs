a85ce8f276dd454639a22bbf54ba2f49da122b49
Richardjurco <113421214+Richardjurco@users.noreply.github.com>
2022-10-06 08:40:06 +0200

    Update Checker.cs

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,17 +1,1 @@
-using System;
 
-namespace Open_Lab_04._02
-{
-    public class Doubler
-    {
-        public string DoubleChar(string original)
-        {
-            string result = string.Empty;
-             for (int a = 0;a < original.Length;a++)
-            {
-                result = result + original[a] + original[a];
-            }return result;
-            
-        }
-    }
-}
