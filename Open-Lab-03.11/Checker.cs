65e6fbfab56f622b67d0614eb9b9c2485c1ecf3e
Richardjurco <113421214+Richardjurco@users.noreply.github.com>
2022-10-06 08:45:57 +0200

    Update Checker.cs

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,1 +1,12 @@
+using System;
 
+namespace Open_Lab_03._11
+{
+    public class Checker
+    {
+        public bool IsPalindrome(string str)
+        {
+            throw new NotImplementedException();
+        } 
+    }
+}
